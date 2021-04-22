08e5b251582ab2a406fb227e3df7ea08f9f922e9
Marie-Camille <mariecamille.dorio@gmail.com>
2021-04-22 15:12:58 +0200

    Quête

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { render } from 'react-dom';
+import { useQuery, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+const EXCHANGE_RATES = gql`
+{
+  launches(limit: 5) {
+    launch_date_utc
+    launch_success
+    rocket {
+      rocket_name
+    }
+    links {
+      video_link
+    }
+    details
+    id
+      }
+    }
+`;
+
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data.launches)
+  
+
+  return data.launches.map(({ launch_date_utc, launch_succes, rocket, links, id , details}) => (
+    <div key={id}>
+      <p>
+        {launch_date_utc}
+      </p>
+      <p>
+        {launch_succes ? 'true' : 'false'}
+      </p>
+      <p>
+        {rocket.rocket_name}
+      </p>
+      <p>
+        {links.video_link}
+      </p>
+      <p>
+        {details}
+      </p>
+    </div>
+  ));
+}
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <h2>My first Apollo app 🚀</h2>
+      
     </div>
   );
 }
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+    <ExchangeRates />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 export default App;
